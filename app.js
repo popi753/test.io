4556a240f79435dc94e5e198fdd05c4b1eda4532
popi753 <luka.popi.919@gmail.com>
2023-02-12 04:54:48 +0400

    commit1 register+schema

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose")
+var User = require("./schema")
+
 
 
 mongoose.set('strictQuery', true)
@@ -33,7 +35,8 @@ mongoose.connect(process.env.database,
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -48,7 +51,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose");
+var User = require("../schema")
+var active = false
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res) {
+  res.render('index',);
 });
+
+
+router.get("/login", (req, res)=>{
+  if (active) {
+    res.send("already logged in")
+  } else {
+    res.render("login")
+
+  }
+})
+
+router.get("/register", (req, res)=>{
+  res.render("register")
+})
+
+router.post("/profile", (req, res)=>{
+  const user = new User({
+    username: req.body.Rusername,
+    password: req.body.Rpassword
+  })
+
+
+
+  user.save(async (err,saveduser)=>{
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("nigga saved")
+      active = true
+      res.render("profile", {name : saveduser.username})
+      
+    }
+
+  })
+
+})
 
 module.exports = router;
